fdf8bd2f4caef4d3d6bf58a5c0129c4e4ce26780
MirasXX1 <mirasshokybay06@gmail.com>
2026-02-08 23:52:57 +0500

    Initial commit with full OOP folder

---
+++ practice2/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Student Example");
+        Student stud = new Student("Miras", 1, 2024);
+        stud.displayDetails();
+        stud.incrementID();
+        System.out.println("Posle incrementing ID:");
+        stud.displayDetails();
+        System.out.println();
+
+        System.out.println("StarTriangle Example");
+        StarTriangle triangle = new StarTriangle(4);
+        System.out.println(triangle.toString());
+        System.out.println();
+
+        System.out.println("Uaqyt Mysal");
+        Time t1 = new Time(23, 5, 6);
+        System.out.println("Universal: " + t1.toUniversal());
+        System.out.println("Standard: " + t1.toStandard());
+
+        Time t2 = new Time(4, 24, 33);
+        Time tSum = t1.add(t1, t2);
+        System.out.println("Sum Universal: " + tSum.toUniversal());
+        System.out.println("Sum Standard: " + tSum.toStandard());
+    }
+}
